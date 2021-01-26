89249c618e2dede257fb50168c4a9d35637e78ad
Mehdi <elouakifi.mehdi@gmail.com>
2021-01-26 21:02:31 +0100

    Work Correct until now

---
+++ src/main/java/ma/s/myappws/controllers/UserController.java
@@ -0,0 +1,27 @@
+package ma.s.myappws.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping(path = "/{id}")
+    public String getUser(@PathVariable String id){
+        return "getUser was called" + id;
+    }
+
+
+
+
+    @DeleteMapping
+    public void deleteUser(){
+
+    }
+
+
+    @PutMapping
+    public void updateUser(){
+
+    }
+}
---
+++ src/main/java/ma/s/myappws/shared/dto/UserDto.java
@@ -0,0 +1,89 @@
+package ma.s.myappws.shared.dto;
+
+import java.io.Serializable;
+
+public class UserDto implements Serializable {
+
+    private static final long serialVersionUID = -3913161947240771596L;
+    private Long id;
+    private String userID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String encryptedPassword;
+    private String emailVerificationToken;
+    private Boolean emailVerificationStatus=false;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    public String getEmailVerificationToken() {
+        return emailVerificationToken;
+    }
+
+    public void setEmailVerificationToken(String emailVerificationToken) {
+        this.emailVerificationToken = emailVerificationToken;
+    }
+
+    public Boolean isEmailVerificationStatus() {
+        return emailVerificationStatus;
+    }
+
+    public void setEmailVerificationStatus(boolean emailVerificationStatus) {
+        this.emailVerificationStatus = emailVerificationStatus;
+    }
+}
