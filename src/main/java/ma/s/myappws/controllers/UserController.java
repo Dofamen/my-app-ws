1301d034c1efff4e0b2ab9290ceb0f523d24eb1a
Mehdi <elouakifi.mehdi@gmail.com>
2021-01-26 22:13:06 +0100

    More Coding..

--- src/main/java/ma/s/myappws/controllers/UserController.java
+++ src/main/java/ma/s/myappws/controllers/UserController.java
@@ -1,10 +1,19 @@
 package ma.s.myappws.controllers;
 
+import ma.s.myappws.Requests.UserRequest;
+import ma.s.myappws.Response.UserResponse;
+import ma.s.myappws.services.UserService;
+import ma.s.myappws.shared.dto.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    UserService userService;
 
     @GetMapping(path = "/{id}")
     public String getUser(@PathVariable String id){
@@ -12,6 +21,16 @@ public class UserController {
     }
 
 
+    @PostMapping
+    public UserResponse createUser(@RequestBody UserRequest userRequest){
+        UserDto userDto = new UserDto();
+        BeanUtils.copyProperties(userRequest, userDto);
+        UserDto createUser =  userService.createUser(userDto);
+        UserResponse userResponse = new UserResponse();
+        BeanUtils.copyProperties(createUser, userResponse);
+
+        return  userResponse;
+    }
 
 
 
--- src/main/java/ma/s/myappws/services/impl/UserServiceImpl.java
+++ src/main/java/ma/s/myappws/services/impl/UserServiceImpl.java
@@ -11,9 +11,11 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayDeque;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
