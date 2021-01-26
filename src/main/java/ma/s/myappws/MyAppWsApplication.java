a43e67bf741bf562990dd410d52cb73d7013677c
Mehdi <elouakifi.mehdi@gmail.com>
2021-01-26 20:10:11 +0100

    INIT Project

---
+++ src/main/java/ma/s/myappws/MyAppWsApplication.java
@@ -0,0 +1,13 @@
+package ma.s.myappws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAppWsApplication.class, args);
+    }
+
+}
